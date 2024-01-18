101ce68ee7e05400fc821c7f18eddf72ccb605f3
yashjawale <yash25.j@gmail.com>
2024-01-18 12:41:04 +0530

    Enable streamlit

--- next.config.js
+++ next.config.js
@@ -1,4 +1,14 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  async redirects() {
+    return [
+      {
+        source: '/about',
+        destination: 'https://acunetix11.streamlit.app/',
+        permanent: false
+      },
+    ]
+  },
+}
 
 module.exports = nextConfig
