92465747bafda378ba94d43f5c9c476db4a1b395
yashjawale <yash25.j@gmail.com>
2024-01-21 22:40:30 +0530

    Add analytics & favicons

--- app/layout.jsx
+++ app/layout.jsx
@@ -3,6 +3,8 @@ import './globals.css'
 import Footer from './components/Footer'
 import { STREAMLIT_ENABLE } from './settings'
 
+import { GoogleAnalytics } from '@next/third-parties/google'
+
 const inter = Inter({
   subsets: ['latin'],
   variable: '--font-inter',
@@ -24,7 +26,27 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <head>
-        <link rel="icon" href="/assets/acunetix-logo.png" sizes="any" />
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/apple-touch-icon.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/favicon-16x16.png"
+        />
+        <link rel="manifest" href="/site.webmanifest" />
+        <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#444444" />
+        <meta name="msapplication-TileColor" content="#ffffff" />
+        <meta name="theme-color" content="#ffffff" />
       </head>
       <body
         className={`${inter.variable} ${bungee.variable} ${
@@ -33,6 +55,7 @@ export default function RootLayout({ children }) {
       >
         {children}
         <Footer />
+        <GoogleAnalytics gaId="G-LCPLRME260" />
       </body>
     </html>
   )
